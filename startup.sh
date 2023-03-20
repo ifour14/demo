c09c03f733b2ac2306b610a33b97060991a10000
ifour14 <96166091+ifour14@users.noreply.github.com>
2023-03-20 21:25:32 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-
 echo =================================
 echo  自动化部署脚本启动
 echo =================================
 
 echo 启动项目
-java -jar docker-demo.jar
+java -jar docker-demo.jar &
 echo 项目启动完成
 
