e82455bd0a7db031108d87127db83717da2dec32
if14 <if14@163.com>
2023-03-20 18:01:25 +0800

    add files

---
+++ startup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+echo =================================
+echo  自动化部署脚本启动
+echo =================================
+
+echo 停止原来运行中的工程
+APP_NAME=docker-demo
+
+tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+if [ ${tpid} ]; then
+    echo 'Stop Process...'
+    kill -15 $tpid
+fi
+sleep 2
+tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+if [ ${tpid} ]; then
+    echo 'Kill Process!'
+    kill -9 $tpid
+else
+    echo 'Stop Success!'
+fi
+
+echo 启动项目
+nohup java -jar docker-demo.jar &> /home/ifour/nohup.out &
+echo 项目启动完成
+
